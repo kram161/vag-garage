2916ce74eb616e00b30d45099d2e8773dd28fa96
Farid <kram5051@mail.ru>
2024-10-12 23:42:40 +0300

    dev

---
+++ src/js/main.js
@@ -0,0 +1,85 @@
+/**
+ * !(i)
+ * Код попадает в итоговый файл, только когда вызвана функция, например FLSFunctions.spollers();
+ * Или когда импортирован весь файл, например import "files/script.js";
+ * Неиспользуемый код в итоговый файл не попадает.
+
+ * Если мы хотим добавить модуль следует его раскомментировать
+ */
+// import { MousePRLX } from './libs/parallaxMouse'
+// import AOS from 'aos'
+// import Swiper, { Navigation, Pagination } from 'swiper';
+
+import { BaseHelpers } from './helpers/base-helpers';
+import { PopupManager } from './modules/popup-manager';
+import { BurgerMenu } from './modules/burger-menu';
+import { Tabs } from './modules/tabs';
+import { Accordion } from './modules/accordion';
+
+BaseHelpers.checkWebpSupport();
+
+BaseHelpers.calcScrollbarWidth();
+
+BaseHelpers.addTouchClass();
+
+BaseHelpers.addLoadedClass();
+
+BaseHelpers.headerFixed();
+
+/**
+ * Открытие/закрытие модальных окон
+ * Чтобы модальное окно открывалось и закрывалось
+ * На окно повешай атрибут data-popup="<название окна>"
+ * На кнопку, которая вызывает окно повешай атрибут data-type="<название окна>"
+
+ * На обертку(.popup) окна добавь атрибут '[data-close-overlay]'
+ * На кнопку для закрытия окна добавь класс '.button-close'
+ * */
+new PopupManager();
+
+/**
+ *  Модуль для работы с меню (Бургер)
+ * */
+new BurgerMenu().init();
+
+/**
+ *  Библиотека для анимаций
+ *  документация: https://michalsnik.github.io/aos
+ * */
+// AOS.init();
+
+/**
+ * Параллакс мышей
+ * */
+// new MousePRLX();
+
+new Tabs('tabs-example', {
+	onChange: (data) => {
+		console.log(data);
+	},
+});
+
+new Accordion('.accordion', {
+	shouldOpenAll: false, // true
+	defaultOpen: [], // [0,1]
+	collapsedClass: 'open',
+});
+
+
+import Swiper from '../static/swiper/swiper-bundle.min.mjs'
+
+
+const presentationFirst = new Swiper('.about-us__slider', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.about-us__next',
+		prevEl: '.about-us__prev',
+	},
+	slidesPerView: 4,
+	spaceBetween: 12
+
+});
+
